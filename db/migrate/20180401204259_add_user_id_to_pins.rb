bafcb477e9213766fa3528d2b8c679d8a51775ed
raksheen <raksheenayaz@gmail.com>
2018-04-02 17:07:18 -0400

    added image uploading with PaperClip. Still can't get root localhost 3000 file path to work as homepage

--- db/migrate/20180401204259_add_user_id_to_pins.rb
+++ db/migrate/20180401204259_add_user_id_to_pins.rb
@@ -1,4 +1,5 @@
 class AddUserIdToPins < ActiveRecord::Migration[5.1]
+  # this is made by typing rails g migration add_user_id_to_pins user_id:integer:index
   def change
     add_column :pins, :user_id, :integer
     add_index :pins, :user_id
